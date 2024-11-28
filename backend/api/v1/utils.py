97b8b007850ac9cca2eca46480dc81dbae88b1ff
Chriss Di <diyc22@mails.tsinghua.edu.cn>
2024-11-28 10:09:23 +0800

    密码机制初步

--- backend/api/v1/utils.py
+++ backend/api/v1/utils.py
@@ -91,3 +91,13 @@ def login_required(func):
             return func(*args, **kwargs)
 
     return wrapper
+
+def verify_password(user_id, password):
+    """
+    校验密码
+    :param user_id: 用户id
+    :param password: 密码
+    :return: bool
+    """
+    
+    return True
