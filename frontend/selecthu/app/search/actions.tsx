7d9dac97e6e3f2ef3396223717c07cd8a5ed4d64
ubuntu <ubuntu@localhost.localdomain>
2024-12-09 11:37:55 +0800

    fix: issues concerning server actions
    喜报：现在在搜索页面的课程名搜索输入任意内容并点击搜索可以看到 console 里输出 403 了！

--- frontend/selecthu/app/search/actions.tsx
+++ frontend/selecthu/app/search/actions.tsx
@@ -1,8 +1,12 @@
 "use server"
 
 export async function searchCourses() {
-    const res = await fetch("http://localhost:8000/api/v1/courses/")
-    return res.status
+    try {
+        const res = await fetch("http://backend:8001/api/v1/courses/")
+        return res.status
+    } catch (error) {
+        return error.message
+    }
     // const json = await res.json()
 
     // return json
